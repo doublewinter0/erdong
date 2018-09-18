9e012b3e643b7dcfd110eddf638e64816c59b31c
doublewinter0 <trasepi@gmail.com>
2018-09-19 00:44:11 +0800

    add interfaces

--- src/main/java/com/trasepi/entertain/Fraudulent.java
+++ src/main/java/com/trasepi/entertain/Fraudulent.java
@@ -1,8 +1,61 @@
 package com.trasepi.entertain;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * 模拟诈金花游戏, 来验证一些特殊牌出现的概率
  */
 public class Fraudulent {
+
+    public static void main(String[] args) {
+        byte[] pokers = init();
+        // System.out.println(Arrays.toString(pokers));
+    }
+
+    private static boolean isFlush(byte[] bts) {
+        return false;
+    }
+
+    private static boolean isStraight(byte[] bts) {
+        return false;
+    }
+
+    private static boolean isLeopard(byte[] bts) {
+        return false;
+    }
+
+    // initialize
+    private static byte[] init() {
+
+        byte length = 52;
+        byte subLength = 13;
+        byte interval = 20;
+
+        // 所有牌
+        byte[] pokers = new byte[length];
+        // 红桃
+        byte[] hearts = new byte[subLength];
+        // 黑桃
+        byte[] spades = new byte[subLength];
+        // 方片
+        byte[] diamonds = new byte[subLength];
+        // 草花
+        byte[] clubs = new byte[subLength];
+
+        for (byte i = 0; i < subLength; i++) {
+            hearts[i] += (i + 1);
+            spades[i] += (i + 1 + interval);
+            diamonds[i] += (i + 1 + interval * 2);
+            clubs[i] += (i + 1 + interval * 3);
+
+            pokers[i] = hearts[i];
+            pokers[i + subLength] = spades[i];
+            pokers[i + subLength * 2] = diamonds[i];
+            pokers[i + subLength * 3] = clubs[i];
+        }
+        return pokers;
+    }
 
 }
--- src/main/java/com/trasepi/entertain/Poker.java
+++ src/main/java/com/trasepi/entertain/Poker.java
@@ -1,21 +1,45 @@
+/*
 package com.trasepi.entertain;
 
+*/
 /**
  * 扑克牌实体
- */
+ *//*
 
+// @Data
 public class Poker {
 
-    // 红桃
-    private byte[] hearts;
+    private Poker() {}
 
-    // 黑桃
-    private byte[] spades;
+    public byte[] init() {
 
-    // 方片
-    private byte[] diamonds;
+        byte length = 52;
+        byte subLength = 13;
+        byte interval = 20;
 
-    // 草花
-    private byte[] club;
+        // 所有牌
+        byte[] pokers = new byte[length];
+        // 红桃
+        byte[] hearts = new byte[subLength];
+        // 黑桃
+        byte[] spades = new byte[subLength];
+        // 方片
+        byte[] diamonds = new byte[subLength];
+        // 草花
+        byte[] clubs = new byte[subLength];
 
+        for (byte i = 0; i < subLength; i++) {
+            hearts[i] += (i + 1);
+            spades[i] += (i + 1 + interval);
+            diamonds[i] += (i + 1 + interval * 2);
+            clubs[i] += (i + 1 + interval * 3);
+
+            pokers[i] = hearts[i];
+            pokers[i + subLength] = spades[i];
+            pokers[i + subLength * 2] = diamonds[i];
+            pokers[i + subLength * 3] = clubs[i];
+        }
+        return pokers;
+    }
 }
+*/
