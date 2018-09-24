5162466b3773fcdeba4d998ed7ff765148e8da65
doublewinter0 <trasepi@gmail.com>
2018-09-24 16:43:26 +0800

    简化代码 只考虑一种情况

--- src/main/java/com/trasepi/entertain/Fraudulent.java
+++ src/main/java/com/trasepi/entertain/Fraudulent.java
@@ -8,29 +8,15 @@ import java.util.Arrays;
  * 模拟诈金花游戏, 来验证一些特殊牌出现的概率
  */
 public class Fraudulent {
-
-    static {
-        CARDINAL = 3;
-        LENGTH = 52;
-        SUBLENGTH = 13;
-        INTERVAL = 20;
-    }
-
-    private static final int CARDINAL;
-    private static final int LENGTH;
-    private static final int SUBLENGTH;
-    private static final int INTERVAL;
-
     private static long count4StraightFlush;
     private static long count4Leopard;
 
     public static void main(String[] args) {
-        // int[] pokers = init();
-        // System.out.println(Arrays.toString(pokers));
         int tabCardsNum = 4;
         int allCardsNum = 52;
         long begin = System.currentTimeMillis();
-        for (int s = 1; s <=  1000000000; s++) {
+
+        for (int s = 1; s <= 1000000000; s++) {
             int[][] cards = dealCards(tabCardsNum, allCardsNum);
             for (int[] card : cards) {
                 if (isStraightFlush(card)) count4StraightFlush++;
@@ -40,7 +26,7 @@ public class Fraudulent {
                 System.out.println("count4StraightFlush = " + count4StraightFlush);
                 System.out.println("      count4Leopard = " + count4Leopard);
                 long semiend = System.currentTimeMillis();
-                System.out.println("进度: " + (s / 10000000)+ "%, 已耗时: " + (semiend - begin) / 1000.0D + " 秒");
+                System.out.println("已完成 : " + (s / 10000000) + "%, 已耗时: " + IMath.timeFormat(semiend - begin));
             }
         }
     }
@@ -48,16 +34,19 @@ public class Fraudulent {
     // 同花
     private static boolean isFlush(int[] cardinal) {
         if (cardinal.length != 3) throw new IllegalArgumentException("Please Intput Correct Args!");
+
         Arrays.sort(cardinal);
-        boolean isHearts = cardinal[cardinal.length - 1] <= SUBLENGTH;
-        boolean isSpades = cardinal[0] > INTERVAL && cardinal[2] <= (INTERVAL + SUBLENGTH);
-        boolean isDiamonds = cardinal[0] > INTERVAL * 2 && cardinal[2] <= (INTERVAL * 2 + SUBLENGTH);
-        boolean isClubs = cardinal[0] > INTERVAL * 3 && cardinal[2] <= (INTERVAL * 3 + SUBLENGTH);
+        boolean isHearts = cardinal[2] <= 13;
+        boolean isSpades = cardinal[0] > 20 && cardinal[2] <= (20 + 13);
+        boolean isDiamonds = cardinal[0] > 20 * 2 && cardinal[2] <= (20 * 2 + 13);
+        boolean isClubs = cardinal[0] > 20 * 3 && cardinal[2] <= (20 * 3 + 13);
         return isHearts || isSpades || isDiamonds || isClubs;
     }
 
     // 顺子
     private static boolean isStraight(int[] cardinal) {
+        if (cardinal.length != 3) throw new IllegalArgumentException("Please Intput Correct Args!");
+
         residue(cardinal);
         Arrays.sort(cardinal);
         boolean general = (cardinal[0] + 1 == cardinal[1]) && (cardinal[1] + 1 == cardinal[2]);
@@ -72,13 +61,15 @@ public class Fraudulent {
 
     // 豹子
     private static boolean isLeopard(int[] cardinal) {
+        if (cardinal.length != 3) throw new IllegalArgumentException("Please Intput Correct Args!");
+
         residue(cardinal);
         return (cardinal[0] == cardinal[1]) && (cardinal[1] == cardinal[2]);
     }
 
     private static void residue(int[] cardinal) {
-        if (cardinal.length != 3) throw new IllegalArgumentException("Please Intput Correct Args!");
-        for (int i = 0; i < 3; i++) {
+        int length = cardinal.length;
+        for (int i = 0; i < length; i++) {
             cardinal[i] = cardinal[i] % 20;
         }
     }
@@ -86,51 +77,52 @@ public class Fraudulent {
     // 模拟发牌
     private static int[][] dealCards(int tabCardsNum, int allCardsNum) {
         int handCardsNum = allCardsNum - tabCardsNum;
-        if ((handCardsNum) % (CARDINAL * 2) != 0) throw new IllegalArgumentException("Please Intput Correct Args!");
-        int[] initCards = init();
+        if ((handCardsNum) % (3 * 2) != 0) throw new IllegalArgumentException("Please Intput Correct Args!");
+        int[] initCards = initCards();
         int[] ranseq = IMath.getNonRRS(handCardsNum, allCardsNum, true);
-        int ranseqLength = ranseq.length;
-        int[] handCards = new int[ranseqLength];
-        for (int i = 0; i < ranseqLength; i++) {
+        int[] handCards = new int[handCardsNum];
+        for (int i = 0; i < handCardsNum; i++) {
             handCards[i] = initCards[ranseq[i]];
         }
-        int round = handCardsNum / CARDINAL;
-        int[][] handCardss = new int[round][CARDINAL];
+
+        int round = handCardsNum / 3;
+        int[][] handCardss = new int[round][3];
         for (int i = 0; i < handCardss.length; i++) {
             if (handCardss[i].length >= 0) {
-                System.arraycopy(handCards, CARDINAL * i, handCardss[i], 0, handCardss[i].length);
-                Arrays.sort(handCardss[i]);
+                System.arraycopy(handCards, 3 * i, handCardss[i], 0, handCardss[i].length);
             }
         }
         return handCardss;
     }
 
     // 初始化
-    private static int[] init() {
-
+    private static int[] initCards() {
         // 所有牌
-        int[] pokers = new int[LENGTH];
-        // 红桃
-        int[] hearts = new int[SUBLENGTH];
-        // 黑桃
-        int[] spades = new int[SUBLENGTH];
-        // 方片
-        int[] diamonds = new int[SUBLENGTH];
-        // 草花
-        int[] clubs = new int[SUBLENGTH];
+        int[] pokers = new int[52];
 
-        for (int i = 0; i < SUBLENGTH; i++) {
+        // 红桃
+        int[] hearts = new int[13];
+
+        // 黑桃
+        int[] spades = new int[13];
+
+        // 方片
+        int[] diamonds = new int[13];
+
+        // 草花
+        int[] clubs = new int[13];
+
+        for (int i = 0; i < 13; i++) {
             hearts[i] += (i + 1);
-            spades[i] += (i + 1 + INTERVAL);
-            diamonds[i] += (i + 1 + INTERVAL * 2);
-            clubs[i] += (i + 1 + INTERVAL * 3);
+            spades[i] += (i + 1 + 20);
+            diamonds[i] += (i + 1 + 20 * 2);
+            clubs[i] += (i + 1 + 20 * 3);
 
             pokers[i] = hearts[i];
-            pokers[i + SUBLENGTH] = spades[i];
-            pokers[i + SUBLENGTH * 2] = diamonds[i];
-            pokers[i + SUBLENGTH * 3] = clubs[i];
+            pokers[i + 13] = spades[i];
+            pokers[i + 13 * 2] = diamonds[i];
+            pokers[i + 13 * 3] = clubs[i];
         }
         return pokers;
     }
-
 }
